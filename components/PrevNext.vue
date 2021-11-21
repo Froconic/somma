1b944bedaee7c15b8560d5919ae03218e32c3128
Froconic <froconicdesign@gmail.com>
2021-11-20 20:25:35 -0700

    flipped the prev and next need to fix the order of the links

--- components/PrevNext.vue
+++ components/PrevNext.vue
@@ -2,31 +2,32 @@
   <div class="container-fluid">
     <div class="row">
       <div class="col-6 text-center">
-    <NuxtLink v-if="prev" :to="{name: 'blog-slug', params: { slug: prev.slug }}" class="font-bold text-primary hover:underline">Previous: {{ prev.title }}
-    </NuxtLink>
-    <span v-else>End of the line</span>
+        <NuxtLink v-if="next" :to="{name: 'blog-slug', params: { slug:next.slug }}" class="font-bold text-primary hover:underline">Next: {{ next.title }}
+        </NuxtLink>
+        <span v-else>End of the line</span>
       </div>
       <div class="col-6 text-center">
-    <NuxtLink v-if="next" :to="{name: 'blog-slug', params: { slug:next.slug }}" class="font-bold text-primary hover:underline">Next: {{ next.title }}
-    </NuxtLink>
-    <span v-else>End of the line</span>
+        <NuxtLink v-if="prev" :to="{name: 'blog-slug', params: { slug: prev.slug }}" class="font-bold text-primary hover:underline">Previous: {{ prev.title }}
+        </NuxtLink>
+        <span v-else>End of the line</span>
       </div>
+
     </div>
   </div>
 </template>
 
 <script>
   export default {
-props: {
-  prev: {
-    type: Object,
-    default: () => null
-  },
-  next: {
-    type: Object,
-    default: () => null
-  }
-}
+    props: {
+      prev: {
+        type: Object,
+        default: () => null
+      },
+      next: {
+        type: Object,
+        default: () => null
+      }
+    }
   }
 
 </script>
