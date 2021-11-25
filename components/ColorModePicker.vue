1d716fbdfe7365bdbafcef4e70fbde6739a6b362
Froconic <froconicdesign@gmail.com>
2021-11-25 11:30:03 -0700

    added colormode now fixing functionality

---
+++ components/ColorModePicker.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <ColorScheme placeholder="..." tag="span">
+    Color mode: <b>{{ $colorMode.preference }}</b>
+    <span v-if="$colorMode.preference === 'system'">(<i>{{ $colorMode.value }}</i> mode detected)</span>
+  </ColorScheme>
+    <h1>Color mode: {{ $colorMode.value }}</h1>
+    <select v-model="$colorMode.preference" @click="change">
+      <option v-for="color of colors" :key="color" value="color.value">{{color.value}}</option>
+    </select>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      colors: ['system','light','dark','blue']
+    }
+  },
+  methods: {
+    //change color mode on click
+    change(event) {
+      this.$store.commit('changeColorMode', event.target.value)
+    }
+
+  }
+}
+
+</script>
